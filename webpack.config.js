5e0c8f6a3084a63bda69fee65166abae5e72f1cc
Jhordy Gavinchu <dev@jhordyess.com>
2022-01-30 22:12:14 -0400

    Move public to docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 module.exports = {
   entry: path.resolve(__dirname, "./src/index.jsx"),
   output: {
-    path: path.resolve(__dirname, "./public"),
+    path: path.resolve(__dirname, "./docs"),
     filename: "main.js",
   },
   module: {
