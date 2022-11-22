0e6f1ae7f395bbd6887d23d90bae7009ee780c03
Jhordy Gavinchu <dev@jhordyess.com>
2022-11-21 21:49:14 -0400

    Branch change, dockerize scripts in docker branch

--- docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cp -av ./public/ ./docker/public/
-cp -av ./src/ ./docker/src/
-cp -v ./package.json ./docker/package.json
-cp -v ./webpack.config.js ./docker/webpack.config.js
-docker build -t jhordyess/conoge ./docker &&
-  docker run -d --rm -p 80:80 jhordyess/conoge
-rm -rv docker/public docker/src docker/package.json docker/webpack.config.js
